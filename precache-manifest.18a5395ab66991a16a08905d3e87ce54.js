034dc7255fc13f40f01a705a6eaf4493169301c8
viktoryia.budzionnaya.epam <Viktoryia_Budzionnaya@epam.com>
2019-03-01 18:49:58 +0300

    Updates

--- precache-manifest.18a5395ab66991a16a08905d3e87ce54.js
+++ precache-manifest.18a5395ab66991a16a08905d3e87ce54.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/react-pet/static/js/runtime~main.d3e7fdd1.js"
   },
   {
-    "revision": "399b75d137c51391ef68",
-    "url": "/react-pet/static/js/main.399b75d1.chunk.js"
+    "revision": "2ccf6d8ba0a4fa5ccc28",
+    "url": "/react-pet/static/js/main.2ccf6d8b.chunk.js"
   },
   {
     "revision": "6001b0f596d71e649fe1",
     "url": "/react-pet/static/js/2.6001b0f5.chunk.js"
   },
   {
-    "revision": "399b75d137c51391ef68",
+    "revision": "2ccf6d8ba0a4fa5ccc28",
     "url": "/react-pet/static/css/main.d77235fc.chunk.css"
   },
   {
-    "revision": "245ef2f2bd98c3e65c7578e0a4b73209",
+    "revision": "74594132a69de16ecd52100a34da0868",
     "url": "/react-pet/index.html"
   }
 ];
