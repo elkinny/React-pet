83832b2d929dfa057441563a258140e7358be10c
Viktoryia Budzionnaya <Viktoryia_Budzionnaya@epam.com>
2020-07-14 14:22:00 +0300

    Updates

---
+++ precache-manifest.39ac282f3414d5a66806b79fc7f7bf24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17bb85c1e7fe84dab4db8835fd8140e",
+    "url": "/React-to-do/index.html"
+  },
+  {
+    "revision": "5f568bd01a853b244d10",
+    "url": "/React-to-do/static/css/main.2b9edea1.chunk.css"
+  },
+  {
+    "revision": "105a46f043e57862f604",
+    "url": "/React-to-do/static/js/2.52aa1c4d.chunk.js"
+  },
+  {
+    "revision": "09b8665a3e34f587e27a040e9046a33b",
+    "url": "/React-to-do/static/js/2.52aa1c4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f568bd01a853b244d10",
+    "url": "/React-to-do/static/js/main.b429918a.chunk.js"
+  },
+  {
+    "revision": "77ca26fc02c553975202",
+    "url": "/React-to-do/static/js/runtime-main.b44e78cc.js"
+  }
+]);
